e4eca9aca5480d53f3cf527ca8091a666dd0c4b7
joshplantin <joshplantin@gmail.com>
2022-12-20 18:28:42 -0600

    the basic solar system with the feature sun added is ready to be deployed

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        solarSystem xron=new solarSystem();
+
+        xron.moon=2;
+        xron.planets=2;
+        xron.sun=1;
+
+        FeatureSun fxron= new FeatureSun();
+        fxron.color="laksdjf";
+        fxron.size="2349519283";
+        fxron.temp="239834";
+    }
+}
